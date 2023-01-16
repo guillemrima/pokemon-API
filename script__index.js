d82059cdbc7acf9ecd2934daf21b5aa3b57d05bb
guillemrima <rivasmartorellguillem@gmail.com>
2023-01-16 11:28:37 +0100

    Declared const of pokemon API

--- script__index.js
+++ script__index.js
@@ -4,8 +4,29 @@ const formularioPokemon = document.getElementById("formulario__pokemon");
 const escogerPokemon = (data) => {
     data.preventDefault();
     var pokemon = document.getElementById("input__pokemon").value;
+
+    var pokeAPI = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    fetch(pokeAPI).then(res =>{
+        res.json().then(data =>{
+            console.log(data)
+        })
+    })
+
     console.log(pokemon);
     formularioPokemon.reset();
 }
+
+
+
+//PREGUNTAR COMO PUEDO USAR LA VAR POKEMON EN OTRA FUNCION
+//const generarPokeAPI = () =>{
+//     var pokeAPI = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+//    fetch(pokeAPI).then(res =>{
+//        res.json(),then(data =>{
+//            console.log(data)
+//            hydratePokemon(data);
+//        })
+//    })
+//}
 
 formularioPokemon.addEventListener("submit", escogerPokemon);
